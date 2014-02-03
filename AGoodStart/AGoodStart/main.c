6daf9040fec3c871e2f8ff7d9a1de884fc9134ff
cokeefe <chaemobile@gmail.com>
2014-02-02 20:38:23 -0500

    chpt 2

---
+++ AGoodStart/AGoodStart/main.c
@@ -0,0 +1,21 @@
+//
+//  main.c
+//  AGoodStart
+//
+//  Created by Chae O'Keefe on 2/2/14.
+//  Copyright (c) 2014 com.chaeokay. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+    // Print the beginning of the novel
+    printf("It was the best of times.\n");
+    printf("It was the worst of times.\n");
+    /*  Is that actually any good?
+        Maybe it needs a rewrite. */
+    
+    return 0;
+}
+
