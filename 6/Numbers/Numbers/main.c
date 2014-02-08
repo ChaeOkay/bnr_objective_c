b0a98ad7963ab860606c05c75bc77a9d2763f2ad
cokeefe <chaemobile@gmail.com>
2014-02-08 15:29:00 -0500

    chpt 6 Integer Division

--- 6/Numbers/Numbers/main.c
+++ 6/Numbers/Numbers/main.c
@@ -11,6 +11,8 @@
 int main(int argc, const char * argv[])
 {
     printf("3 * 3 + 5 * 2 = %d\n", 3 * 3 + 5 * 2);
+    printf("11 / 3 = %d remainder of %d \n", 11 / 3, 11 % 3);
+    printf("11 / 3.0 = %f\n", 11 / (float)3);
     return 0;
 }
 
