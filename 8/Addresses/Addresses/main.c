1385f14009b1d0fcd28cdb50433a583b3cf35044
cokeefe <chaemobile@gmail.com>
2014-02-08 16:36:22 -0500

    chpt 8 Getting addresses

---
+++ 8/Addresses/Addresses/main.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  Addresses
+//
+//  Created by Chae O'Keefe on 2/8/14.
+//  Copyright (c) 2014 chaeokay.com. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+    int i = 17;
+    int *addressOfI = &i;
+    printf("i stores its value at %p\n", addressOfI);
+    
+    return 0;
+}
+
