0673072ac34c414689ad602108c4c7b025b3beec
cokeefe <chaemobile@gmail.com>
2014-02-08 16:21:08 -0500

    chpt 7 Challenge

--- 7/Coolness/Coolness/main.c
+++ 7/Coolness/Coolness/main.c
@@ -3,60 +3,33 @@
 //  Coolness
 //
 //  Created by Chae O'Keefe on 2/8/14.
-//  Copyright (c) 2014 com.chaeokay. All rights reserved.
+//  Copyright (c) 2014 chaeokay.com. All rights reserved.
 //
 
 #include <stdio.h>
 
+void sayNumber(int i)
+{
+    printf("%d\n", i);
+}
+
+void multipleOfFive()
+{
+    printf("Found one!\n");
+}
+
 int main(int argc, const char * argv[])
 {
-    int i = 0;
- 
-    // while loop
-    while (i < 12) {
-        printf("Aaron is Cool\n");
-        i ++;
-    }
-    
-    // for loop
-    for (i = 0; i < 12; i++) {
-        printf("%d. Aaron is Cool\n", i);
-    }
-    
-    // breaking a loop
-    int j;
-    for (j = 0; j < 12; j++){
-        printf("Checking j = %d\n", j);
-        
-        if (j + 90 == j * j) {
-            break;
-        }
-    }
-    
-    printf("The answer is %d\n", j);
-    
-    // skip when k is 3
-    int k;
-    for (k = 0; k < 12; k++){
-        if (k % 3 == 0){
+    for (int i = 99; i >= 0; i--) {
+        if (i % 3 != 0 ){
             continue;
         }
-        printf("Checking k = %d\n", k);
-        if (k + 90 == k * k){
-            break;
+        
+        sayNumber(i);
+        
+        if (i % 5 == 0) {
+            multipleOfFive();
         }
     }
-    printf("The answer is %d.\n", k);
-    
-    // do while loop
-    
-    int l = 0;
-    do {
-        printf("%d. Aaron is Cool\n", l);
-        l++;
-    } while (l < 13);
-    
-    
-    return 0;
 }
 
