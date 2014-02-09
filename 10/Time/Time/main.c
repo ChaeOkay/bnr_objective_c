f68e1fcbae9a31fbbbc2c6e2d00ce3ca29a8ccc3
cokeefe <chaemobile@gmail.com>
2014-02-09 09:58:30 -0500

    chpt 10 Challenge - refactor

--- 10/Time/Time/main.c
+++ 10/Time/Time/main.c
@@ -13,26 +13,20 @@ typedef struct {
     long timePlus4Mil;
 } Future;
 
-int main(int argc, const char * argv[])
+void returnFutureDate(long timeElapsed)
 {
-//    struct tm now;
-//    long secondsSince1970 = time(NULL);
-//    printf("It has been %ld seconds since 1970\n\n", secondsSince1970);
-//    
-//    localtime_r(&secondsSince1970, &now);
-//    printf("The time is %d:%d:%d\n", now.tm_hour, now.tm_min, now.tm_sec);
-//    printf("The date is %d:%d:%d\n", now.tm_mon + 1, now.tm_mday, now.tm_year + 1900);
-//    printf("\n");
+    struct tm later;
     
     Future future;
-    future.timePlus4Mil = time(NULL) + 4000000;
-    
-    printf("The time since 1970 plus 4,000,000 is %ld seconds \n\n", future.timePlus4Mil);
-    
-    struct tm later;
+    future.timePlus4Mil = time(NULL) + timeElapsed;
     localtime_r(&future.timePlus4Mil, &later);
+    
     printf("The date is %d:%d:%d\n", later.tm_mon + 1, later.tm_mday, later.tm_year + 1900);
-    printf("\n");
+}
+
+int main(int argc, const char * argv[])
+{
+    returnFutureDate(4000000);
     
     return 0;
 }
