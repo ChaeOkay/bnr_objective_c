ce32ea5879277a816ecdead4db3d3d185716cc13
cokeefe <chaemobile@gmail.com>
2014-02-10 20:42:50 -0500

    chpt 13

---
+++ 13/DST/DST/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  DST
+//
+//  Created by Chae O'Keefe on 2/10/14.
+//  Copyright (c) 2014 chaeokay.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
