1ddc83787d62ab1696c17508fd470a011793db56
cokeefe <chaemobile@gmail.com>
2014-02-11 22:19:18 -0500

    chpt 13 Challenge

--- 13/DST/DST/main.m
+++ 13/DST/DST/main.m
@@ -8,14 +8,31 @@
 
 #import <Foundation/Foundation.h>
 
+BOOL findTimeZone()
+{
+    NSTimeZone *myTimeZone = [NSTimeZone systemTimeZone];
+    return [myTimeZone isDaylightSavingTime];
+}
+
+void interpret(int value)
+{
+    if (value == 0) {
+        NSLog(@"NO");
+    } else if (value == 1) {
+        NSLog(@"Yes");
+    } else {
+        NSLog(@"Something went wrong...");
+    }
+    
+}
+
 int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
         
-        // insert code here...
-        NSLog(@"Hello, World!");
-        
+        interpret(findTimeZone());
+
     }
     return 0;
 }
