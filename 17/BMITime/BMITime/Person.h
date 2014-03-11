e4e8cba79586e1c83f4f3a2b80126cb76d0ae0cb
cokeefe <chaemobile@gmail.com>
2014-03-10 21:32:04 -0400

    chpt 17 Refactor

--- 17/BMITime/BMITime/Person.h
+++ 17/BMITime/BMITime/Person.h
@@ -18,13 +18,9 @@
     int weightInKilos;
 }
 
-// You will be able to set those instance variables using these methods
-- (float)heightInMeters;
-- (void)setHeightInMeters:(float)h;
-- (int)weightInKilos;
-- (void)setWeightInKilos:(int)w;
+@property float heightInMeters;
+@property int weightInKilos;
 
-// This method calculates the Body Mass Index
 - (float)bodyMassIndex;
 
 @end
